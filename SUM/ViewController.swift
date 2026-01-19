e26d424a8e8f9eb133427f05d54a3a65dd18ef01
Vamsi Krishna Chintam <110526922+vamsireal@users.noreply.github.com>
2026-01-19 15:54:35 -0500

    Created and designed an UI app for adding two numbers Added Main Screen and added View Controller

--- SUM/ViewController.swift
+++ SUM/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// MARK: Input Variables
+    @IBOutlet weak var Input1: UITextField!
+    @IBOutlet weak var Input2: UITextField!
+    @IBOutlet weak var Result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: Logic section for TwoSum
+    @IBAction func ResultofSum(_ sender: Any) {
+        let Input1:Int? = Int(Input1.text!)
+        let Input2:Int? = Int(Input2.text!)
+                
+                if let Input1 = Input1, let Input2 = Input2 {
+                    let sum:Int = Input1 + Input2
+                    Result.text = "\(sum)"
+                } else {
+                    Result.text = "Please enter valid numbers"
+                }
+    }
 }
+
+
+
 
